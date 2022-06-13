ea3919956c0f91239ae055811ca56c43e4f80a57
Josediaz777 <manueltiscareno1200@gmail.com>
2022-06-13 18:46:09 -0500

    feat:We aggregate the calculator witth the function for basic operations

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,12 @@ function App() {
   ]);
   const [name, setName] = useState("");
   const [lastName, setLastname] = useState("");
+  //Const Calculadora
+  const [numero1, setNumero1] = useState(0);
+  const [numero2, setNumero2] = useState(0);
+  const [resultado, setResult] = useState(0);
+
+
 
   return (
     <div className="App">
@@ -39,14 +45,17 @@ function App() {
           setLastname(event.target.value);
         }}
       />
-      <br /><br />
+      <br /><br />      <br /><br /><br /><br /><b></b>
+
       <button className='agregar'
         onClick={() => {
           setPeople((current) => [{ name, lastName }, ...current]);
         }}
       >
-        Add user
+        Agregar Usuario
       </button>
+
+      <br /><br /><br /><br /><b></b>
 
       <hr className='espacio' />
       <h2>Tabla de Usuarios</h2>
@@ -56,7 +65,75 @@ function App() {
           <li className='tabla' id={idx}>{`${person.name} ${person.lastName}`}</li>
         ))}
       </ul>
+      <br />
+      <hr />
+      <div className=''><h2>CALCULADORA</h2></div>
+      <div>
+        <input
+          type="number"
+          name="Número 1"
+          className='name'
+          placeholder='Ingresa el numero 1'
+          onChange={(event) => {
+            setNumero1(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          name="Número 2"
+          className='name'
+          placeholder='Ingresa el numero 2'
+          onChange={(event) => {
+            setNumero2(event.target.value);
+          }}
+        />
+        <h3>Resultado: {resultado}</h3>
+      </div>
+      <div className='padding-bottom'>
+        <button
+          onClick={() => {
+            setResult(((+numero1) + (+numero2)));
+          }}
+          className='operacion'
+        >
+          Sumar
+        </button>
+        <button
+          onClick={() => {
+            setResult(((+numero1) - (+numero2)));
+          }}
+          className='operacion'
+
+        >
+          Resta
+        </button>
+        <button
+          onClick={() => {
+            setResult(((+numero1) * (+numero2)));
+          }}
+          className='operacion'
+        >
+          Multiplicar
+        </button>
+        <button
+          onClick={() => {
+            if (numero2 == 0) {
+              window.alert('No es posible dividir entre 0, Ingresa un número válido')
+              return;
+            }
+            setResult(((+numero1) / (+numero2)));
+          }}
+          className='operacion'
+        >
+          Dividir
+        </button>
+      </div>
+      {/* <p>{resultado}</p> */}
     </div>
+
+
+
+
   );
 }
 
